690d5a8a02b707b7b601f0c1ca70d9ed13cb45c8
Abhishek B <itsabhichrishere@gmail.com>
2023-10-24 19:50:41 +0530

    Logger & Exception Handling completed

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from Housing.logger import logging
+from Housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("We are testing Custom Exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
     return "Starting Machine Learning End to End Project"
 
 
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 
 # Declaring variables for setup function.
 Project_Name = "house-predictor"
-Version = "0.0.1"
+Version = "0.0.2"
 Author = "Abhishek B"
 Description = "This is the House Price Prediction End to End Machine Learning Project"
 Packages = ["Housing"]
@@ -21,14 +21,14 @@ def get_requirements_list()->List[str]:
     of libraries mentioned in requirements.txt file.
     """
     with open(REQUIREMENTS_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
     name=Project_Name,
     version=Version,
     author=Author,
     description=Description,
-    packages=Packages, 
+    packages=find_packages(), 
     install_requires = get_requirements_list()
 
 )
